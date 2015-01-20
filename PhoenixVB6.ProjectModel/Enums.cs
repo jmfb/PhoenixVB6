8c387c4528159b7b95883afbb972536616f513cb
Marcin Polewski <marcin.polewski@gmail.com>
2015-01-20 02:02:08 -0600

    Added resharper settings

--- PhoenixVB6.ProjectModel/Enums.cs
+++ PhoenixVB6.ProjectModel/Enums.cs
@@ -1,58 +1,87 @@
-﻿using System.ComponentModel;
+﻿#region Copyright Preamble
+
+// 
+//    Copyright 2015 Overman Group, LLC.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+using System.ComponentModel;
 
 namespace PhoenixVB6.ProjectModel
 {
+	// TODO: Update casing and spelling of enum identifiers so that suppressions are no longer required.
+
 	public enum ProjectType
 	{
-		[Description("Standard EXE")]
-		Exe,
+		[Description("Standard EXE")] Exe,
 
-		[Description("ActiveX EXE")]
-		OleExe,
+		[Description("ActiveX EXE")] OleExe,
 
-		[Description("ActiveX DLL")]
-		OleDll,
+		[Description("ActiveX DLL")] OleDll,
 
-		[Description("ActiveX Control")]
-		Control,
+		[Description("ActiveX Control")] Control
 	}
 
 	public enum DataBindingBehavior
 	{
 		None = 0,
 		SimpleBound = 1,
-		ComplexBound = 2,
+		ComplexBound = 2
 	}
 
 	public enum DataSourceBehavior
 	{
 		None = 0,
-		DataSource = 1,
+		DataSource = 1
 	}
 
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1008:EnumsShouldHaveZeroValue")]
 	public enum Instancing
 	{
 		Private = 1,
 		PublicNotCreatable = 2,
 		SingleUse = 3,
 		GlobalSingleUse = 4,
-		MultiUse = 5,
-		GlobalMultiUse = 6,
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly",
+			MessageId = "MultiUse")] [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly",
+				MessageId = "Multi")] MultiUse = 5,
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly",
+			MessageId = "MultiUse")] [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly",
+				MessageId = "Multi")] GlobalMultiUse = 6
 	}
 
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly",
+		MessageId = "MTS")]
+	// ReSharper disable once InconsistentNaming
 	public enum MTSTransactionMode
 	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly",
+			MessageId = "MTS")]
+		// ReSharper disable once InconsistentNaming
 		NotAnMTSObject = 0,
 		NoTransactions = 1,
 		RequiresTransaction = 2,
 		UsesTransaction = 3,
-		RequiresNewTransaction = 4,
+		RequiresNewTransaction = 4
 	}
 
 	public enum Persistable
 	{
 		NotPersistable = 0,
-		Persistable = 1,
+		Persistable = 1
 	}
-
 }
--- PhoenixVB6.ProjectModel/IProjectItem.cs
+++ PhoenixVB6.ProjectModel/IProjectItem.cs
@@ -1,4 +1,23 @@
-﻿namespace PhoenixVB6.ProjectModel
+﻿#region Copyright Preamble
+
+// 
+//    Copyright 2015 Overman Group, LLC.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+namespace PhoenixVB6.ProjectModel
 {
 	public interface IProjectItem
 	{
@@ -7,14 +26,5 @@
 		string Key { get; }
 
 		string Value { get; }
-	}
-
-	public interface IReferenceProjectItem : IProjectItem
-	{
-	}
-
-	public interface IFileProjectItem : IProjectItem
-	{
-		string RelativePath { get; }
 	}
 }
